7704ecfb67d1f56093afa995fd89160acb74eab5
Juan Ignacio Casal <ignacio.casal@auto1.com>
2017-07-21 12:23:01 +0200

    Making tests fail again

--- test/index.js
+++ test/index.js
@@ -2,8 +2,7 @@ var assert = require('assert');
 
 describe('Test', function () {
 	it('should not be falsy', function () {
-		assert.equal(true, true);
-		assert.equal(true, true);	
+		assert.equal(false, true);
 	});
 
 	
